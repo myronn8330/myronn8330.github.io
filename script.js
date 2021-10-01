4013762de908e4249cb5881d9ac55df900b94985
myronn8330 <91744974+myronn8330@users.noreply.github.com>
2021-10-01 11:20:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Myron Ng"
+let userName = "myronn8300";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
